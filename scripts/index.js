c0fbb91e9464b343642528a553eac12bdc9ec286
Alina Lebedeva <chebary@gmail.com>
2024-06-25 13:02:57 +0300

    Добавлены функции создания и удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+function createCard(cardData, deleteCallback) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
-// @todo: Функция создания карточки
+    cardElement.querySelector('.card__image').src = cardData.link;
+    cardElement.querySelector('.card__image').alt = cardData.name;
+    cardElement.querySelector('.card__title').textContent = cardData.name;
 
-// @todo: Функция удаления карточки
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', deleteCallback);
 
-// @todo: Вывести карточки на страницу
+    return cardElement;
+};
+
+function deleteCard(event) {
+    const deleteButton = event.target;
+    const placeItem = deleteButton.closest('.places__item');
+    placeItem.remove();
+};
+
+const placesList = document.querySelector('.places__list');
+
+initialCards.forEach(function (item) {
+    const card = createCard(item, deleteCard);
+    placesList.append(card);
+});
+
+
